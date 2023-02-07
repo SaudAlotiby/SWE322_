a30c9dff5035a439d0321c0c640a0e3cf48db6df
SaudAlotiby <98090254+SaudAlotiby@users.noreply.github.com>
2023-02-07 15:39:57 +0300

    Add files via upload

---
+++ lap_2.php
@@ -0,0 +1,29 @@
+<?php
+
+echo "<h1> saud alotiby & 201711080 </h1>";
+
+function add_scores ($score1, $score2){ 
+return ($score1 + $score2);
+}
+
+echo add_scores(3,4);
+
+function subtract_scores ($score1, $score2){ 
+return ($x-$y);
+}
+
+echo subtract_scores(4,3);
+function multiply_scores ($score1, $score2){ 
+return ($x * $y);
+}
+
+echo multiply_scores(3,4);
+
+function divide_scores ($score1, $score2){ 
+return ($x / $y);
+}
+
+echo divide_scores(3,4);
+
+
+?>
