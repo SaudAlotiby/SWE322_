63437a937bc69e949b1baee04a8e177692019590
SaudAlotiby <98090254+SaudAlotiby@users.noreply.github.com>
2023-02-07 15:29:07 +0300

    Add files via upload

---
+++ lap_1sa.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "<h1> saud alotiby & 201711080 </h1>";
+
+echo $n = 1;
+echo $s = "1";
+echo $n1 = $n.$s;
+echo $n2 = $n1 . "10 little penguins";
+echo $n3 = "hello";
+echo $n4 = " world";
+echo $n5 = $n3.$n4;
+echo $n6 = $n3.$n4;
+?>
